76805157b7e96b8ded4494f055fdaf9b2607fafd
Silvano <s.norberti@dstech.it>
2023-06-23 13:43:04 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:globo_fitness/screens/intro_screen.dart';
+
+void main() {
+  runApp(GlobeApp());
+}
+
+class GlobeApp extends StatelessWidget {
+  const GlobeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: IntroScreen(),
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+    );
+  }
+}
